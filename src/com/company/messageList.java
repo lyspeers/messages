00fdb9d36057e5f1cbaf1b26eb994598e2cbd700
ls059 <lyspers@gmail.com>
2018-02-05 10:21:55 -0500

    initial commit

--- src/com/company/messageList.java
+++ src/com/company/messageList.java
@@ -1,7 +1,16 @@
 package com.company;
-
+ import java.util.*;
 /**
  * Created by ls059 on 2/5/18.
  */
 public class messageList {
+    static Map<String, String[]> messages = new HashMap<>();
+    public messageList(){
+
+    }
+
+    void newChat(String n, String m){
+
+    }
+
 }
