dde906c91887f2454b46ac8294adacccc19c9829
Luke <Lyspeers@gmail.com>
2018-02-05 19:27:01 -0500

    messeges

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        messageList.newChat("jack", "hey");
+        System.out.println(messageList.messages.get("jack"));
+        while(true){
+            Scanner use = new Scanner(System.in);
+            System.out.println("Would you like to : Start a new message(start), or continue a chat(continue)");
+            String mode = use.next();
+            if(mode.equalsIgnoreCase("start")){
+
+            }else if(mode.equalsIgnoreCase("continue")){
+
+            }else{
+                System.out.println("Please input the correct operation.");
+            }
+        }
     }
 }
--- src/com/company/messageList.java
+++ src/com/company/messageList.java
@@ -4,13 +4,36 @@ package com.company;
  * Created by ls059 on 2/5/18.
  */
 public class messageList {
-    static Map<String, String[]> messages = new HashMap<>();
+    //static String g[] = {"hi"};
+    //static String h[] = {"hello"};
+
+    static Map<String, ArrayList<String>> messages = new HashMap<>();
     public messageList(){
 
     }
 
-    void newChat(String n, String m){
+    static void newChat(String n, String m){
 
+        messages.put(n, new ArrayList<>());
+        messages.get(n).add(m);
+    }
+    static void oldChat(String n){
+        for(int i = messages.get(n).size(); i > 0; i--){
+            System.out.println(messages.get(n).get(i));
+        }
+    }
+    private void chat(String n){
+
+        Scanner sender = new Scanner(System.in);
+        System.out.println("What is the message you would like to send");
+        String s = sender.nextLine();
+
+        Scanner message = new Scanner(System.in);
+        System.out.println("Enter your message here");
+        String m = message.nextLine();
+
+        String c = s + " :" + m;
+        messages.get(n).add(c);
     }
 
 }
